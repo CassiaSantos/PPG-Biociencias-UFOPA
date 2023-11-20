2498a51a1ce4f66f484894b68eaceb375ed919ea
Cássia Oliveira dos Santos <cassia.oliveira.profissional@gmail.com>
2023-11-20 18:16:58 -0300

    top bar

--- assets/js/header.js
+++ assets/js/header.js
@@ -6,21 +6,25 @@ document.addEventListener("DOMContentLoaded", function () {
     const headerHTML = `
 
         
-     <section id="topbar1" class="topbar1 d-flex align-items-center">
-            <div class="container d-flex justify-content-center justify-content-md-between"><!---->
+     <section id="topbar" class="topbar d-flex align-items-center">
+            <div class="container d-flex justify-content-center">
                 <div class="contact-info d-flex align-items-center">
                     <i class="bi bi-envelope d-flex align-items-center"><a  href="mailto:ppgbio@ufopa.edu.br">e-mail: ppgbio@ufopa.edu.br</a></i>
                 </div>
-                <div class="social-links d-none d-md-flex align-items-center">
+                <!--<div class="social-links d-none d-md-flex align-items-center">
                     <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
                     <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
                     <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
                     <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
-                </div>
+                </div>-->
             </div>
         </section>  <!--End Top Bar -->
 
         <header id="header" class="header d-flex align-items-center">
+
+            <!--<div class="top">
+                <p><a  href="mailto:ppgbio@ufopa.edu.br">e-mail: ppgbio@ufopa.edu.br</a></p>
+            </div>-->
             <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
                 <a href="index.html" class="logo d-flex align-items-center">
                     <!-- <img src="assets/img/logoBio.png" alt=""> -->
