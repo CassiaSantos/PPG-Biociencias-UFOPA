2c28c12bf6a07fcb8858422a319aaca7422f0b0b
Cássia Oliveira dos Santos <cassia.oliveira.profissional@gmail.com>
2023-12-06 18:07:25 -0300

    idiomas

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,14 +78,14 @@ document.addEventListener('DOMContentLoaded', () => {
   window.addEventListener('load', navbarlinksActive);
   document.addEventListener('scroll', navbarlinksActive);
 
-  // window.gtranslateSettings = {
-  //   "default_language":"pt",
-  //   "detect_browser_language":true,
-  //   "languages":["pt","en","es"],
-  //   "wrapper_selector":".gtranslate_wrapper",
-  //   "flag_style":"3d",
-  //   "alt_flags":{"en":"usa","pt":"brazil"}
-  // }
+  window.gtranslateSettings = {
+    "default_language":"pt",
+    "detect_browser_language":true,
+    "languages":["pt","en","es"],
+    "wrapper_selector":".gtranslate_wrapper",
+    "flag_style":"3d",
+    "alt_flags":{"en":"usa","pt":"brazil"}
+  }
 
   /**
    * visualização da senha.
