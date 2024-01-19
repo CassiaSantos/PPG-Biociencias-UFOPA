de91aa6ae7ca9d7fc5d13fac644487adcab8151a
Fer96carvalho <f.luiz1996@gmail.com>
2024-01-19 16:33:49 -0300

    commit

--- assets/js/script-corpoAcademico.js
+++ assets/js/script-corpoAcademico.js
@@ -1,20 +1,19 @@
 const teamSection = document.getElementById("div-prof");
 
-// Função para preencher os membros com os dados do JSON
 function preencherMembros() {
-    const corpoAcademico = "https://apippgbio-or3c.vercel.app/corpo-academico";
-    fetch(corpoAcademico)
+  const corpoAcademico = "https://apippgbio-or3c.vercel.app/corpo-academico";
+  fetch(corpoAcademico)
     .then((response) => response.json())
     .then((data) => {
-      data.slice(0, 4).forEach((membro, index) => {          
-          const memberDiv = document.createElement("div");
-          memberDiv.className = "col-xl-3 col-md-6 d-flex justify-content-center ";
-          memberDiv.dataset.aos = "fade-up";
-          memberDiv.dataset.aosDelay = 100 * (index + 1);
-        
-          memberDiv.innerHTML = `
-            <div class="member">
-              <img src="assets/img/team/${membro.foto}" class="img-fluid" alt="">
+      data.slice(0, 4).forEach((membro, index) => {
+        const memberDiv = document.createElement("div");
+        memberDiv.className = "col-xl-3 col-md-6 d-flex justify-content-center ";
+        memberDiv.dataset.aos = "fade-up";
+        memberDiv.dataset.aosDelay = 100 * (index + 1);
+
+        memberDiv.innerHTML = `
+            <div class="member" >
+              <img src="${membro.foto}" class="img-fluid" alt="">
               <h4>${membro.nomeCompleto}</h4>
               <span>${membro.cargo}</span>
               <div class="social">
@@ -23,15 +22,14 @@ function preencherMembros() {
                 <a href="${membro.redesSociais.instagram}"><i class="bi bi-instagram"></i></a>
                 <a href="${membro.redesSociais.linkedin}"><i class="bi bi-linkedin"></i></a>
               </div>
-              <a href="${membro.curriculoLattes}" class="readmore stretched-link">Currículo Lattes <i class="bi bi-arrow-right"></i></a>
+              <a href="${membro.curriculoLattes}" class="readmore">Currículo Lattes <i class="bi bi-arrow-right"></i></a>
             </div>
           `;
-        
-          teamSection.appendChild(memberDiv);
+
+        teamSection.appendChild(memberDiv);
       });
-      })
+    })
 
 }
 
-// Chame a função para preencher os membros quando o documento estiver pronto
 document.addEventListener("DOMContentLoaded", preencherMembros);
